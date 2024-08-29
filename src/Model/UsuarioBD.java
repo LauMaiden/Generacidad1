2750f293b23a6ed5d097e42e0d90ea38bcaf2651
User <User@DESKTOP-RQ831MC>
2024-08-28 22:18:54 -0500

    genericidad

---
+++ src/Model/UsuarioBD.java
@@ -0,0 +1,38 @@
+package Model;
+
+import Genericos.Pair;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UsuarioBD {
+    
+    private Connection conn;
+    
+    public UsuarioBD() {
+        ConnectBD connx = new ConnectBD();
+        conn = connx.getConnection();
+    }
+    
+    public void InsertaUsuario(String id, Pair uno, Pair dos){
+        try {
+             
+            String sql = "INSERT INTO usuariog(id,nombre,edad, eps, fecha" 
+             + ") VALUES(?,?,?,?,?)";
+            try(
+                 PreparedStatement stm = conn.prepareStatement(sql)){
+                 stm.setString(1, id);
+                 stm.setObject(2,uno.getFirst());
+                 stm.setObject(3,uno.getSecond());                 
+                 stm.setObject(4, dos.getFirst());
+                 stm.setObject(5, dos.getSecond());                 
+                 stm.execute();
+                if(stm != null)
+                    stm.close();                             
+            }
+        }   catch (SQLException e) {
+             throw new RuntimeException();
+            }
+     }
+     
+    }
---
+++ src/principal/Registro.java
@@ -0,0 +1,142 @@
+
+package principal;
+
+import Genericos.Pair;
+import Model.UsuarioBD;
+import java.awt.Frame;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.time.LocalDate;
+import javax.swing.JOptionPane;
+
+
+public class Registro extends javax.swing.JFrame {
+
+    
+    public Registro() {
+        initComponents();
+        this.setTitle("ejercicio generico");        
+        this.setLocationRelativeTo(null);
+    }
+
+    private void limpia(){
+        txtid.setText("");
+        txtnombre.setText("");
+        txtedad.setText("");
+        txteps.setText("");
+        txtfecha.setText("");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+        txtid = new javax.swing.JTextField();
+        txtnombre = new javax.swing.JTextField();
+        txtedad = new javax.swing.JTextField();
+        txteps = new javax.swing.JTextField();
+        txtfecha = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(425, 305));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setText("Registrar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 230, -1, -1));
+        getContentPane().add(txtid, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 70, 100, -1));
+        getContentPane().add(txtnombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 100, 100, -1));
+        getContentPane().add(txtedad, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 130, 110, -1));
+        getContentPane().add(txteps, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 160, 100, -1));
+        getContentPane().add(txtfecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 190, 90, -1));
+
+        jLabel1.setText("EPS");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 160, -1, -1));
+
+        jLabel2.setText("Id");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 80, -1, -1));
+
+        jLabel3.setText("Nombre");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 110, -1, -1));
+
+        jLabel4.setText("Edad");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 130, -1, -1));
+
+        jLabel5.setText("Fecha de Nacimiento");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 180, -1, 40));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        //llama a insertar
+        Pair<String, Integer> Dato1 = new Pair<String, Integer>(txtnombre.getText(),Integer.parseInt(txtedad.getText()));
+        LocalDate fecha = LocalDate.parse(txtfecha.getText());        
+        Pair<String, LocalDate> Dato2 = new Pair<String, LocalDate>(txteps.getText(),fecha);       
+        UsuarioBD xx = new UsuarioBD();
+        xx.InsertaUsuario(txtid.getText(), Dato1, Dato2);
+        JOptionPane.showMessageDialog(null, "Usuario insertado con exito","Informacion", JOptionPane.INFORMATION_MESSAGE);
+        System.out.println("usuario insertado");
+        limpia();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Registro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Registro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Registro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Registro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Registro().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTextField txtedad;
+    private javax.swing.JTextField txteps;
+    private javax.swing.JTextField txtfecha;
+    private javax.swing.JTextField txtid;
+    private javax.swing.JTextField txtnombre;
+    // End of variables declaration//GEN-END:variables
+}
